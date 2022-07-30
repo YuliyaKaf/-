e5a1629c271011dcf56039e0465d140a1077e147
«Yuliya» <umenshikova.95@gmail.com>
2022-07-30 12:52:52 +0500

    Подкорректировала вывод строк

--- Program.cs
+++ Program.cs
@@ -3,43 +3,35 @@ int count;
 string s;
 string[] ArrString2; 
 
-    Console.WriteLine("Введите несколько строк. Как только захотите закончить ввод нажмите 2 раза клавишу Enter:");
-     count = 0; 
-     ArrString = new string[count];
+Console.WriteLine("Введите несколько строк. Как только захотите закончить ввод нажмите 2 раза клавишу Enter:");
+count = 0; 
+ArrString = new string[count];
 
-      do
-      {
-               s = Console.ReadLine();
+  do
+  {
+    s = Console.ReadLine();
         
-        if (s!="")
-        {
-          count++;
-          ArrString2 = new string[count];
+    if (s!="")
+    {
+    count++;
+    ArrString2 = new string[count];
 
-          
-          for (int i = 0; i < ArrString2.Length - 1; i++)
-            ArrString2[i] = ArrString[i];
-
-          
-          ArrString2[count - 1] = s;
-
-          
-          ArrString = ArrString2;
+    for (int i = 0; i < ArrString2.Length - 1; i++)
+        ArrString2[i] = ArrString[i];
+        ArrString2[count - 1] = s;
+        ArrString = ArrString2;
         }
       } while (s != "");
 
-      
-// for (int i=0; i<ArrString.Length; i++)
-// {
-//     Console.Write($" {ArrString[i]}");
-// }
 
-for (int i = 0; i < ArrString.Length; i++)
-    if (ArrString.Length < 4)
+    for (int i = 0; i < ArrString.Length; i++)
+      if (ArrString.Length <= 3)
     {
-      Console.WriteLine( ArrString[i]);
-      Console.ReadKey();
+    Console.WriteLine($"  {ArrString[i]}"); 
     }
+      
+
+    
         
 
 
