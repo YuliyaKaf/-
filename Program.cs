52ea7d42b1f470b9e60f78a547175b3a9bd5810c
«Yuliya» <umenshikova.95@gmail.com>
2022-07-30 11:09:43 +0500

    Сделала ввод массива строк

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ArrString; 
+int count; 
+string s;
+string[] ArrString2; 
+
+    Console.WriteLine("Введите несколько строк:");
+     count = 0; 
+     ArrString = new string[count];
+
+      do
+      {
+               s = Console.ReadLine();
+        
+        if (s!="")
+        {
+          count++;
+          ArrString2 = new string[count];
+
+          
+          for (int i = 0; i < ArrString2.Length - 1; i++)
+            ArrString2[i] = ArrString[i];
+
+          
+          ArrString2[count - 1] = s;
+
+          
+          ArrString = ArrString2;
+        }
+      } while (s != "");
+
+      
+for (int i = 0; i < ArrString.Length; i++)
+        if (ArrString.Length <= 3)
+        {
+Console.WriteLine(ArrString[i]);
+        }
+        
+      
+      
